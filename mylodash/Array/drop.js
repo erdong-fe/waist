597248f4872e3ace6ce1327f9b49b37481892a75
陈智广 <chenzhiguang02@meituan.com>
2021-04-16 11:36:30 +0800

    feat: Array.drop

--- mylodash/Array/drop.js
+++ mylodash/Array/drop.js
@@ -1,3 +1,14 @@
 function drop(arr, n) {
-    
+    const protoToString = Object.prototype.toString
+    if (protoToString.call(arr) !== '[object Array]') {
+        if (protoToString.call(arr) === '[object String]') {
+            arr = arr.split('');
+        } else {
+            arr = [];
+        }
+    }
+    n = n === undefined ? 1 : n;
+    return arr.slice(n);
 }
+
+module.exports = drop;
--- mylodash/demo.js
+++ mylodash/demo.js
@@ -1,9 +1,30 @@
 const get = require('./Object/get');
+const drop = require('./Array/drop');
 
 /**
- * 测试get方法
+ * 测试Object.get方法
  */
-const object = { 'a': [{ 'b': { 'c': 3 } }] };
-console.log(get(object, 'a[0].b.c'));
-console.log(get(object, ['a', '0', 'b', 'c']));
-console.log(get(object, 'a.b.c', 'default'));
+// const object = { 'a': [{ 'b': { 'c': 3 } }] };
+// console.log(get(object, 'a[0].b.c'));
+// console.log(get(object, ['a', '0', 'b', 'c']));
+// console.log(get(object, 'a.b.c', 'default'));
+
+/**
+ * 测试Array.drop方法
+ */
+const arr = [1,2,3,4];
+const obj = {name: 'drop'};
+const str = '123 4';
+const nullObj = null;
+const undefObj = undefined;
+console.log(drop(arr));
+console.log(drop(arr, 2));
+console.log(drop(arr, 5));
+console.log(drop(obj, 5));
+console.log(drop(str));
+console.log(drop(str, 1));
+console.log(drop(str, 6));
+console.log(drop(nullObj));
+console.log(drop(nullObj, 2));
+console.log(drop(undefObj));
+console.log(drop(undefObj,2 ));
